ffd0721e798a394cac35eebabf0f515e29d72003
Katy Dattoma <katy.dattoma@gmail.com>
2016-05-19 15:41:10 +1000

    conf

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
 
   get '/' => 'home#index'
   resources :events
+  
 
 
   # ================ api controllers return JSON=====================
     namespace :api do
       resources :events
+      resources :types
     end
 end
