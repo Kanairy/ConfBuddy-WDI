af1512f977f6e077494c101c14e321f0addb72bc
HMAN1911 <aydin.harun.23@gmail.com>
2016-05-19 15:39:32 +1000

    add routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   # ================ api controllers return JSON=====================
     namespace :api do
       resources :events
+      resources :users
     end
 end
