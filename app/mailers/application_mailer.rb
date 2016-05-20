622c94ce1c2c57e379b2924d118139258c230650
Katy Dattoma <katy.dattoma@gmail.com>
2016-05-20 14:52:53 +1000

    deleted dev.log

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com" #sender email
+  default from: "confbuddy@gmail.com" 
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,17 +1,17 @@
 class UserMailer < ApplicationMailer
 
-
   def match_buddy(user)
     @user = user
     # @event = event associate to the user
     # @buddy = user associate to the user
-    mail to: @user.email, subject: 'Matching buddy'
+    mail( to: @user.email,
+    subject: 'Matching buddy' )
   end
 end
 
 # this is going to be added into the user_controller
 # into the create method
-# UserMailer.match_buddy(user).deliver_later
+# UserMailer.match_buddy(@user).deliver_later
 
 #options available
 # Notifier.welcome(User.first).deliver_later
