e7f9a04c494935a32704447c3b18fc6fd144cdeb
Katy Dattoma <katy.dattoma@gmail.com>
2016-05-21 11:38:55 +1000

    qr code into mails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,21 @@
 class UserMailer < ApplicationMailer
 
-  def match_buddy(user)
+  def match_buddy(user, event, buddy)
     @user = user
-    # @event = event
-    # @buddy = buddy
+    @event = event
+    @buddy = buddy
+
+    attachments.inline['qr.png'] = File.read('./app/assets/images/qr.png')
     mail( to: @user.email,
     subject: 'Matching buddy' )
   end
 end
+
+# this is going to be added into the user_controller
+# into the create method
+# UserMailer.match_buddy(@user).deliver_later
+
+#options available
+# Notifier.welcome(User.first).deliver_later
+# Notifier.welcome(User.first).deliver_later(wait: 1.hour)
+# Notifier.welcome(User.first).deliver_later(wait_until: 10.hours.from_now)
