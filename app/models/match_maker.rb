b786e40577a1976b85f500cc8cac893305e67421
Wolf <contact@nickwolf.com.au>
2016-05-24 17:29:16 +1000

    adds perfect match functionality

--- app/models/match_maker.rb
+++ app/models/match_maker.rb
@@ -1,30 +1,51 @@
 class MatchMaker
 
   def perfect_match(event, user)
-  if user.get_buddy(event)
-  array = event
-  array.each_with_index do |attendee, index|
-    if attendee.get_buddy(event) == nil
-    #checks for perfect match
-      if user[:strength] == attendee[:seeking] && user[:seeking] == attendee[:strength]
-        match = Match.new
-        match
-    #delete match from array
-    
-
-      return attendee
+    if user.get_buddy(event.id)
+      return false
     end
-  end
-  return false
-end
-
-def match(user, array)
-  if perfect_match(user, array) != false
-    return perfect_match(user, array)
-  else
+    array = event.users
     array.each do |attendee|
+      if attendee.get_buddy(event) == nil
+    #checks for perfect match
+        if user[:strength] == attendee[:seeking] && user[:seeking] == attendee[:strength]
+        match = Match.create(user_one_id:"#{user.id}", user_two_id:"#{attendee.id}", event_id:"#{event.id}")
+    #delete match from array
+        end
+      # return attendee
+      end
     end
   end
-end
+
+  def perfect_match(event, user)
+    if user.get_buddy(event.id)
+      return false
+    end
+    array = event.users
+    array.each do |attendee|
+      if attendee.get_buddy(event) == nil
+    #checks for perfect match
+        if user[:strength] == attendee[:seeking] || user[:seeking] == attendee[:strength]
+        match = Match.create(user_one_id:"#{user.id}", user_two_id:"#{attendee.id}", event_id:"#{event.id}")
+    #delete match from array
+        end
+      # return attendee
+      end
+    end
+  end  
+
+
+
+
+  def match(user, array)
+    if perfect_match(user, array) != false
+      return perfect_match(user, array)
+    else
+      array.each do |attendee|
+      end
+    end
+  end
 
 end
+
+
