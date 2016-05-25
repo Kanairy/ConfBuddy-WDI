4f5729cd2760593adf909590f7c8be85da82f7c5
Alistair Martin <ajmartin_12@hotmail.com>
2016-05-25 15:16:25 +1000

    Added Cards Template in Handle Bars

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
 
   get '/' => 'home#index'
   get '/signup' => 'signup#signup'
-<<<<<<< HEAD
+
   get '/login/oauth2' => 'signup#oauth2'
   get '/login/oatoken' => 'signup#oatoken'
 
-=======
   get '/profile' => 'profile#profile'
->>>>>>> 6159e70f28e8a7edc70b7d7a41d008d6229cb875
+
   resources :events
 
 
