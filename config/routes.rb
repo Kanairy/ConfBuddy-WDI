b0e896197301ec974f50a4bc38a4e41423d0b527
HMAN1911 <aydin.harun.23@gmail.com>
2016-05-25 14:29:25 +1000

    linkedin ruby integration

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
 
   get '/' => 'home#index'
   get '/signup' => 'signup#signup'
-<<<<<<< HEAD
+
   get '/login/oauth2' => 'signup#oauth2'
   get '/login/oatoken' => 'signup#oatoken'
 
-=======
   get '/profile' => 'profile#profile'
->>>>>>> 6159e70f28e8a7edc70b7d7a41d008d6229cb875
   resources :events
 
 
@@ -18,6 +16,5 @@ Rails.application.routes.draw do
       resources :events
       resources :types
       resources :users
-
     end
 end
